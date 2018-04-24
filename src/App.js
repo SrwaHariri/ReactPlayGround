b3886f6c508b6afb4e4bc7196f9a570a0c99d701
srwa hariri <srwa@ematix.com>
2018-04-24 14:49:16 +0200

    adding props to component , passing it through state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "bestoun", age: 34 },
+      { name: "srwa", age: 28 },
+      { name: "parycka", age: 3 }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1> Hi, iam areact app</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        >
+          i want to be a ballerina
+        </Person>
       </div>
     );
     /* return React.createElement(
