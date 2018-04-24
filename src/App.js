96cc3629d9c4c90aa6df0ae373bbf952c1e961dc
srwa hariri <srwa@ematix.com>
2018-04-24 11:30:37 +0200

    returning a react object and calling a create element method on it

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1> Hi, iam areact app</h1>
-      </div>
+    // return (
+    //   <div className="App">
+    //     <h1> Hi, iam areact app</h1>
+    //   </div>
+    // );
+    return React.createElement(
+      "div",
+      { className: "App" },
+      React.createElement("h1", null, "yolo")
     );
   }
 }
