6355c39a5a81e391d87f782ae5376a1867be3b5d
srwa hariri <srwa@ematix.com>
2018-04-24 13:08:12 +0200

    importing custom commponent that outputs dynamic content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1> Hi, iam areact app</h1>
-    //   </div>
-    // );
-    return React.createElement(
-      "div",
-      { className: "App" },
-      React.createElement("h1", null, "yolo")
+    return (
+      <div className="App">
+        <h1> Hi, iam areact app</h1>
+        <Person />
+        <Person />
+        <Person />
+      </div>
     );
+    /* return React.createElement(
+       "div",
+       { className: "App" },
+       React.createElement("h1", null, "yolo")
+    );
+    */
   }
 }
 
