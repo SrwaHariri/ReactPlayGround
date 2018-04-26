b9d33d2fec146926ed41632f73836068fc9ccf0e
srwa hariri <srwa@ematix.com>
2018-04-26 12:50:22 +0200

    using two way bindng ,event handlers ,passing values to components and  ways of changing state of the app examples

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,10 +2,11 @@ import React from "react";
 const person = props => {
   return (
     <div>
-      <p>
+      <p onClick={props.blik} className={props.className}>
         Hi, my name is {props.name} and iam {props.age} years old
       </p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };
