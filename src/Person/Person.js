1241da0cc397e5d33be49a110d34ea3302240054
srwa hariri <srwa@ematix.com>
2018-04-27 10:57:38 +0200

    style added to person cards

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,18 @@
 import React from "react";
+import "./Person.css";
 const person = props => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.blik} className={props.className}>
         Hi, my name is {props.name} and iam {props.age} years old
       </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
+      <input
+        className="input"
+        type="text"
+        onChange={props.changed}
+        value={props.name}
+      />
     </div>
   );
 };
