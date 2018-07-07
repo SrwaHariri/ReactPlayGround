4cfd9cc3b9c18e0348503fee8c5415e453a98447
unknown <srwa.hariri@gmail.com>
2018-07-08 00:08:39 +0200

    react scripts added , forms component got seperated

--- src/App.js
+++ src/App.js
@@ -1,75 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import Person from "./Person/Person";
+import Form from "./conditionalForms/Form";
 
 class App extends Component {
-  state = {
-    persons: [
-      { name: "bestoun", age: 34 },
-      { name: "srwa", age: 28 },
-      { name: "parycka", age: 3 }
-    ],
-    showPersons: false
-  };
-  switchNameHandler = newName => {
-    // console.log("clicked!");
-    // DON'T DO THIS: this.state.persons[0].name="someNewName"
-    this.setState({
-      persons: [
-        { name: newName, age: 34 },
-        { name: "daya", age: 28 },
-        { name: "parygull", age: 4 }
-      ]
-    });
-  };
-  nameChangeHandler = event => {
-    this.setState({
-      persons: [
-        { name: "bestoun", age: 34 },
-        { name: event.target.value, age: 28 },
-        { name: "parygull", age: 3 }
-      ]
-    });
-  };
-
-  togglePersonHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  };
   render() {
-    const someStyle = {
-      color: "#f0b27a"
-    };
     return (
-      <div className="App">
-        <h1 style={someStyle}> Hi, iam areact app</h1>
-        <button onClick={this.togglePersonHandler} className="button-style">
-          Switch names
-        </button>
-        {this.state.showPersons === true ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              className="person1"
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              // this is for changing the array index[0] wich is the first person
-              //the bind is for binding th evalue of newName in switch handler method
-              blik={this.switchNameHandler.bind(this, "bawa aziz")}
-              changed={this.nameChangeHandler}
-            />
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            >
-              i want to be a ballerina
-            </Person>
-          </div>
-        ) : null}
-      </div>
+     <div>
+       <Form />
+     </div>
     );
     /* return React.createElement(
        "div",
