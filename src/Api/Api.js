bc77f6f80bb1159c84f4f479fcfa74b1250740e2
unknown <srwa.hariri@gmail.com>
2019-03-28 22:35:24 +0100

    post method

---
+++ src/Api/Api.js
@@ -0,0 +1,31 @@
+
+import React from 'react'
+import axios from 'axios';
+import ApiPost from "../ApiPost/ApiPost";
+class Api extends React.Component {
+
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+        <div>
+      <ul>
+      <h3>getting list of persons using axios get ApI call </h3>
+        { this.state.persons.map(person => <li>{person.name}</li>)}
+      </ul>
+      <ApiPost />
+      </div>
+    )
+  }
+}
+export default Api;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Form from "./conditionalForms/Form";
 import Contact from "./Contact/Contact";
+import Api from "./Api/Api";
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import registerServiceWorker from './registerServiceWorker';
@@ -14,11 +15,14 @@ const routing = (
         <div><Link className="link" to="/">Home</Link></div>
         <div> <Link to="/Form">Forms</Link></div>
         <div> <Link to="/contact">Animations</Link></div>
+        <div> <Link to="/Api">API</Link></div>
      </div>
     
         <Route path="/" component={App} />
         <Route path="/Form" component={Form} />
         <Route path="/contact" component={Contact} />
+        <Route path="/Api" component={Api} />
+        
     
      
       </div>
